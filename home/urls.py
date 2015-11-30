e498bbda846d24992cd0e96ffef6200116acd51d
Martin Kobersky <martinkobersky@gmail.com>
2015-11-30 15:55:55 +0100

    opraven chyba v routovani

--- home/urls.py
+++ home/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^index/$', views.index),
     url(r'^aktuality/$', views.news_from_front),
-    url(r'^aktuality/(?P<article_id>\d)/$', views.article_from_front),
+    url(r'^aktuality/(?P<article_id>\d+)/$', views.article_from_front),
     url(r'^aktuality/(?P<start>\d+)-(?P<end>\d+)/$', views.article_range_from_front),
     url(r'^(?P<section>\w+)/$', views.section),
     url(r'^(?P<section>\w+)/kalendar/$', views.calendar),
