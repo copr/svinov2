a5bd1882e9a83d26dbf04010a21ac6e7eb4161e7
Martin Kobersky <martinkobersky@gmail.com>
2016-12-29 19:29:22 +0100

    posilam data o eventu do templatu

--- eticket/views.py
+++ eticket/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.utils.safestring import mark_safe
+import json
+
+from eticket.models import Event
 
 def form(request):
-    return render(request, 'form.html')
+    results = [Event.objects.all()[0]]
+    js_data = mark_safe(serializers.serialize("json",results))
+    return render(request, 'form.html', {'event': js_data})
