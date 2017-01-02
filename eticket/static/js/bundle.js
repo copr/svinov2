bd1f84ab2dbe3348ed2354ad1863d8a9d2b63775
Jakub Rehor <Jakub.Rehor@cz.abb.com>
2017-01-02 19:48:51 +0100

    validation added for empty fields, event name and price

--- eticket/static/js/bundle.js
+++ eticket/static/js/bundle.js
@@ -60,15 +60,24 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	/** @jsx React.DOM */var React = __webpack_require__(2);
+	var Error = __webpack_require__(3);
+	var Udalost = event;
+	var UdalostPole = eventField;
+
 
 	module.exports = React.createClass({displayName: "module.exports",
 		getInitialState: function () {
 			return {
-				name: ""
+				name: "",
+				empty: false,
+				ticketNumber: 1,
+				errors: [],
 			}
 		},
 		nameChange: function (e) {
+
 			this.setState({ name: e.target.value });
+
 		},
 		surnameChange: function (e) {
 			this.setState({ surname: e.target.value });
@@ -80,42 +89,59 @@
 			this.setState({ email: e.target.value });
 		},
 		ticketChange: function (e) {
-			this.setState({ ticket: e.target.value });
+			if (e.target.value < 1 || e.target.value === "") {
+				this.setState({ ticketNumber: 1 })
+			} else {
+				this.setState({ ticketNumber: e.target.value });
+			}
+		},
+		validate: function (e) {
+			var val = e.target.value;
+			e.target.parentElement.classList.remove('has-success', 'has-error','empty');
+			if (val) {
+				e.target.parentElement.classList.add('has-success')
+			} else {
+				e.target.parentElement.classList.add('has-error','empty');
+			}
 		},
 		submit: function (e) {
-			alert(
-				"Name: " + this.state.name +
-				"\nSurname: " + this.state.surname +
-				"\nPhone: " + this.state.phone +
-				"\nEmail: " + this.state.email +
-				"\nTickets: " + this.state.ticket
-				);
-		},
+			$("div.empty").each(function () {
+				e.preventDefault();
+				alert("Vypl� vsechna pole!");
+				return false;
+			});
+
+	},
 		render: function () {
-			return (
+		return (
 			React.createElement("div", null, 
+				React.createElement("div", null, 
+					React.createElement("h3", null, "Udalost: ",  Udalost[0].fields.name), 
+					React.createElement("h3", null, "Cena: ", this.state.ticketNumber * Udalost[0].fields.price, " Kc")
+				), 
 				React.createElement("form", {action: "https://google.com", method: "post", className: "col-xs-6 col-xs-offset-3"}, 
-					React.createElement("div", {className: "input-group"}, 
-						React.createElement("input", {onChange: this.nameChange, value: this.state.name, type: "text", className: "form-control", placeholder: "Jmeno"})
+					React.createElement("div", {className: "form-group empty"}, 
+						React.createElement("input", {onChange: this.nameChange, onBlur: this.validate, value: this.state.name, type: "text", className: "form-control", placeholder: "Jmeno"}), 
+						React.createElement(Error, {field: this.state.empty})
 					), 
-					React.createElement("div", {className: "input-group"}, 
-						React.createElement("input", {onChange: this.surnameChange, value: this.state.surname, type: "text", className: "form-control", placeholder: "Prijmeni"})
+					React.createElement("div", {className: "form-group empty"}, 
+						React.createElement("input", {onChange: this.surnameChange, isValid: "false", onBlur: this.validate, value: this.state.surname, type: "text", className: "form-control", placeholder: "Prijmeni"})
 					), 
-					React.createElement("div", {className: "input-group"}, 
-						React.createElement("input", {onChange: this.phoneChange, type: "tel", value: this.state.phoneNumber, className: "form-control", placeholder: "Telefon"})
+					React.createElement("div", {className: "form-group empty"}, 
+						React.createElement("input", {onChange: this.phoneChange, isValid: "false", onBlur: this.validate, type: "tel", value: this.state.phoneNumber, className: "form-control bfh-phone", "data-format": "+420 ddd ddd ddd", placeholder: "Telefon"})
 					), 
-					React.createElement("div", {className: "input-group"}, 
-						React.createElement("input", {onChange: this.emailChange, type: "email", value: this.state.email, className: "form-control", placeholder: "Email"})
+					React.createElement("div", {className: "form-group empty"}, 
+						React.createElement("input", {onChange: this.emailChange, isValid: "false", onBlur: this.validate, type: "email", value: this.state.email, className: "form-control", placeholder: "Email"})
 					), 
-					React.createElement("div", {className: "input-group"}, 
-						React.createElement("input", {onChange: this.ticketChange, type: "number", value: this.state.ticketNumber, className: "form-control", placeholder: "Pocet listku"})
+					React.createElement("div", {className: "form-group empty"}, 
+						React.createElement("input", {onChange: this.ticketChange, isValid: "false", onBlur: this.validate, type: "number", value: this.state.ticketNumber, className: "form-control", placeholder: "Pocet listku"})
 					), 
 
-					React.createElement("button", {type: "submit", className: "btn-success", onClick: this.submit}, "Submit")
+					React.createElement("button", {type: "submit", onClick: this.submit, className: "btn-success"}, "Submit")
 				)
 			)
 			)
-		}
+	}
 	});
 
 /***/ },
@@ -123,6 +149,25 @@
 /***/ function(module, exports) {
 
 	module.exports = React;
+
+/***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/** @jsx React.DOM */var React = __webpack_require__(2);
+
+	module.exports = React.createClass({displayName: "module.exports",
+		render: function () {
+			var error;
+			if (this.props.field) {
+				error = "Povinne pole!";
+			}
+			return (React.createElement("div", null, 
+						React.createElement("span", null, error)
+			))
+		}
+
+	});
 
 /***/ }
 /******/ ]);
